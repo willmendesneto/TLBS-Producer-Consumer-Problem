ea535f7a2ebad8804b6f78588acfb45c3c0f9842
willmendesneto <willmendesneto@gmail.com>
2013-08-05 01:28:46 -0700

    Initial commit
    Signed-off-by: willmendesneto <willmendesneto@gmail.com>

---
+++ README.md
@@ -0,0 +1,44 @@
+Problema do produtor-consumidor
+
+Em programas multithread muitas vezes há uma divisão de trabalho entre os segmentos. em
+um padrão comum, alguns tópicos são produtores e alguns são consumidores. produtores
+criar itens de algum tipo e adicioná-los a uma estrutura de dados, os consumidores
+remover os itens e processá-los.
+
+Programas event-driven são um bom exemplo. Um "evento" é algo que
+Acontece que requer o programa para responder: o usuário pressiona uma tecla ou movimentos
+do rato, um bloco de dados chega a partir do disco, chega um pacote a partir do
+rede, uma operação pendente concluída.
+
+Sempre que ocorre um evento, um segmento produtor cria um objeto de evento e
+adiciona-lo para o buffer de eventos. Ao mesmo tempo, segmentos de consumo ter eventos fora
+do buffer e processá-los. Neste caso, os consumidores são chamados de "evento
+manipuladores ".
+
+Há várias restrições de sincronização que precisamos cumprir para
+fazer este sistema funcionar corretamente:
+
+Enquanto um item está sendo adicionado ou removido do buffer, o buffer é
+em um estado inconsistente. Portanto, os tópicos devem ter acesso exclusivo a
+o tampão.
+
+• Se um segmento consumidor chega, enquanto o buffer está vazio, ele bloqueia até que um
+produtor adiciona um novo item.
+Suponha que os produtores de realizar as seguintes operações mais e mais:
+
+* Código 1;
+    event = waitForEvent()
+    buffer.add(event)
+
+
+Além disso, assume que o consumidor realizar as seguintes operações:
+
+* Código 2;
+    event = buffer.get()
+    event.process()
+
+Como especificado acima, o acesso ao tampão tem de ser exclusivas, mas
+waitForEvent e event.process podem ser executados simultaneamente.
+
+Puzzle: Adicionar declarações de sincronização com o código do produtor e do consumidor
+para impor as restrições de sincronização.
---
+++ app/scripts/Consumer.js
@@ -0,0 +1,51 @@
+var Consumer = function(elementListener, listEvents, classItem){
+    var bufferEvents = ['click'];
+    return {
+        $elementListener : $(elementListener),
+        $listEvents : $(listEvents),
+        classItem : classItem,
+        waitForEvent: function (){
+            var self = this.getInstance();
+            $(this.$elementListener).on('click', function(){
+                var chosenEvent = $(this).text();
+                bufferEvents.push( chosenEvent );
+                self.appendElementInList();
+            });
+        },
+        appendElementInList: function(){
+            var countEvents = bufferEvents.length;
+            var self = this.getInstance();
+            //  Removing oldest elements
+            self.$listEvents.find('li').remove();
+
+            var option = new Array();
+            $.each(bufferEvents, function(key, chosenEvent){
+                option[key] = document.createElement('li');
+                //add text node to li element
+                $( option[key] ).html('<a href="javascript:void(0);" data-key="' + key + '" class="' + self.classItem + '">' + key + ' of ' + countEvents + ' - ' + chosenEvent + '</a>');
+                //  Append all the items
+                self.$listEvents.append(option[key]);
+            });
+
+            this.writingEventsInList();
+        },
+        removeElementInList: function(item){
+            bufferEvents.splice(bufferEvents.indexOf(item), 1);
+            this.appendElementInList();
+        },
+        writingEventsInList:  function(){
+            var self = this.getInstance();
+            $('.' + self.classItem).off('click').on('click', function(){
+                self.removeElementInList( $(this).data('key') );
+            });
+        },
+        getInstance:  function(){
+            return this;
+        },
+        init: function(){
+            this.appendElementInList();
+            this.waitForEvent();
+            this.writingEventsInList();
+        },
+    };
+};
